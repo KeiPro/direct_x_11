63d0a60da7607f817d89a4e7ba59b696e17fef61
Kim youngwoo <keiprogramo@gmail.com>
2024-05-24 11:29:43 +0900

    Device 초기화.

--- GameCoding/Game.cpp
+++ GameCoding/Game.cpp
@@ -15,7 +15,19 @@ void Game::Init(HWND hwnd)
 	_width = GWinSizeX;
 	_height = GWinSizeY;
 
+	// AddRef는 우리가 device에 대해서 무언가를 사용할 때 레퍼런스 카운트를 증가시키는 방식.
+	// 레퍼런스 카운터를 자동으로 관리하는 개념이 스마트 포인터 방식이다.
+	// 
+	// AddRef와 Release는 페어가 잘 맞지 않으면 메모리 누수가 일어나게 된다.
+	// 따라서, 우리가 직접 관리하는 방식은 별로 올바른 방법이 아니게 된다.
+	// 
+	// _device->AddRef();
+	// _device->Release();
+
 	// TODO
+	CreateDeviceAndSwapChain();
+	CreateRenderTargetView();
+	SetViewport();
 }
 
 void Game::Update()
@@ -24,4 +36,91 @@ void Game::Update()
 
 void Game::Render()
 {
+	RenderBigin();
+
+	// TODO
+
+	RenderEnd();
+}
+
+void Game::RenderBigin()
+{
+	// OM : Output Merger
+	// 마지막에 전체적으로 GPU가 그림을 그리는 데까지 성공했으면 그 결과물을 이 도화지에다가 그려줘~ 하는 도화지를 건네줘야함.
+	// 그 도화지가 후면 버퍼이고 후면 버퍼를 묘사하기 위해서 우리는 renderTargetView라는 것을 만들어 놨었음.
+	// 1 : 우리가 사용하고 있는 후면버퍼 하나.
+	_deviceContext->OMSetRenderTargets(1, _renderTargetView.GetAddressOf(), nullptr);
+	_deviceContext->ClearRenderTargetView(_renderTargetView.Get(), _clearColor);
+	_deviceContext->RSSetViewports(1, &_viewport);
+}
+
+void Game::RenderEnd()
+{
+	// [ ] <- [ ]
+	// 후면버퍼를 전면버퍼에 복사해달라 그리고 이걸 화면에다가 출력해달라.
+	HRESULT hr = _swapChain->Present(1, 0);
+	CHECK(hr);
+}
+
+void Game::CreateDeviceAndSwapChain()
+{
+	DXGI_SWAP_CHAIN_DESC desc;
+	ZeroMemory(&desc, sizeof(desc));
+	{
+		desc.BufferDesc.Width = _width;
+		desc.BufferDesc.Height = _height;
+		desc.BufferDesc.RefreshRate.Numerator = 60;
+		desc.BufferDesc.RefreshRate.Denominator = 1;
+		desc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+		desc.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+		desc.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+		desc.SampleDesc.Count = 1;
+		desc.SampleDesc.Quality = 0;
+		desc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+		desc.BufferCount = 1;
+		desc.OutputWindow = _hwnd;
+		desc.Windowed = true;
+		desc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+	}
+
+	// 후면 버퍼.
+	HRESULT hr = ::D3D11CreateDeviceAndSwapChain(
+		nullptr,
+		D3D_DRIVER_TYPE_HARDWARE,
+		nullptr,
+		0,
+		nullptr,
+		0,
+		D3D11_SDK_VERSION,
+		&desc,
+		_swapChain.GetAddressOf(),
+		_device.GetAddressOf(),
+		nullptr,
+		_deviceContext.GetAddressOf()
+	);
+
+	CHECK(hr);
+}
+
+// 후면 버퍼를 묘사하는 _renderTargetView를 설정하는 함수.
+void Game::CreateRenderTargetView()
+{
+	HRESULT hr;
+		
+	ComPtr<ID3D11Texture2D> backBuffer = nullptr;
+	hr = _swapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (void**)backBuffer.GetAddressOf());
+	CHECK(hr);
+
+	_device->CreateRenderTargetView(backBuffer.Get(), nullptr, _renderTargetView.GetAddressOf());
+	CHECK(hr);
+}
+
+void Game::SetViewport()
+{
+	_viewport.TopLeftX = 0.f;
+	_viewport.TopLeftY = 0.f;
+	_viewport.Width = static_cast<float>(_width);
+	_viewport.Height = static_cast<float>(_height);
+	_viewport.MinDepth = 0.f;
+	_viewport.MaxDepth = 1.f;
 }
--- GameCoding/Game.h
+++ GameCoding/Game.h
@@ -11,11 +11,52 @@ public:
 	void Render();
 
 private:
+	void RenderBigin();
+	void RenderEnd();
+
+private:
+	void CreateDeviceAndSwapChain();
+	void CreateRenderTargetView();
+	void SetViewport();
+
+private:
 	HWND _hwnd;
 	uint32 _width = 0;
 	uint32 _height = 0;
 
 private:
-	// DX
+
+	// Device & SwapChain
+	// 
+	// ID3D11Device* _device;
+	// ID3D11DeviceContext* _deviceContext;
+	// 위의 ID3D11Device을 스마트 포인터를 사용하기 위해서 ComPtr을 감싸주는 방식을 아래처럼 사용.
+
+	// ComPtr이 하는 것?
+	// 기본적으로는 AddRef()나 Release()를 자동으로 해주는 역할이 있지만, 더 중요한 것은 아래의 내용이다.
+	// 
+	// 현재 수많은 종류의 GPU가 존재하는데 모든 GPU의 스펙을 일일이 비교해가며 맞추는 작업을
+	// 우리가 직접 하는 것은 불가능하다.
+	// 따라서, Com인터페이스라는 것의 핵심은 마이크로소프트에서 만든 DirectX 라이브러리의 
+	// 특정 클래스들만 우리가 작업을 해주면, GPU제품들이 알아서 하드웨어적으로 연결이 되는 인터페이스이다.
+	ComPtr<ID3D11Device> _device = nullptr;
+	ComPtr<ID3D11DeviceContext> _deviceContext = nullptr; // 리소스랑 렌더링 파이프라인을 묶어주는 역할.
+
+	// DXGI란?
+	// DXGI는 렌더링 하는데에 있어 하드웨어를 효율적으로 돌릴 수 있도록 여러 저수준 작업들을 제공해준다.
+	// 또한, 여러가지 공통적인 그래픽 기능성을 처리하는데, 이를테면 화면 모드 전환, 디스플레이 어댑터나 모니터,
+	// 지원되는 디스플레이 모드(해상도, 주사율 등) 같은 그래픽 시스템 정보의 열거 등의 기능을 제공해준다.
+	// 
+	// 우리 모니터에 뭔가 그려주는 더블 버퍼링과 관련된 변수.
+	ComPtr<IDXGISwapChain> _swapChain = nullptr;
+
+	/// RTV(Render Target View)
+	// 우리는 더블 버퍼링을 할 때, 후면 버퍼에다가 뭔가 그려달라는 요청을해야되는 것으로 사용하고 있음.
+	ComPtr<ID3D11RenderTargetView> _renderTargetView; // 후면 버퍼를 묘사하는 존재.
+
+	// Misc
+	D3D11_VIEWPORT _viewport = { 0 }; // 화면의 크기를 묘사
+	float _clearColor[4] = { 0.5f, 0.5f, 0.5f, 0.5f };
+
 };
 
